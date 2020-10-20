075427f14e229c2b92604e392bf32981340513dc
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-10-20 21:54:33 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-def change
-    create_table :songs do |t|
-      t.string :title
-      t.integer :release_year
-      t.boolean :released
-      t.string :genre
-      t.string :artist_name
+Rails.application.routes.draw do
+  get '/songs', to: 'songs#index'
+  get '/songs/new', to: 'songs#new', as: 'new_song'
+  post '/songs', to: 'songs#create'
+  get '/songs/:id', to: 'songs#show', as: 'song'
+  get '/songs/:id/edit', to: 'songs#edit', as: 'edit_song'
+  patch '/songs/:id', to: 'songs#update'
+  delete '/songs/:id', to: 'songs#destroy'
 
-      t.timestamps null: false
-    end
-  end
+  root 'songs#index'
+end
